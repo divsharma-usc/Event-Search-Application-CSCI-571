559dc4f8d89e8ce154139e7c2ff7b1389507a504
Divyansh Sharma <sharmadi@usc.edu>
2023-03-02 00:42:23 -0800

    Added Auto Complete

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+// Material Modules
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FavoritesComponent } from './favorites/favorites.component';
 import { EventSearchComponent } from './event-search/event-search.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,7 +26,15 @@ import { EventSearchComponent } from './event-search/event-search.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatAutocompleteModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/event-search/event-search.component.ts
+++ frontend/src/app/event-search/event-search.component.ts
@@ -1,10 +1,77 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { debounceTime, tap, switchMap, finalize, distinctUntilChanged, filter } from 'rxjs/operators';
 
 @Component({
-  selector: 'event-search',
-  templateUrl: './event-search.component.html',
-  styleUrls: ['./event-search.component.css']
+    selector: 'event-search',
+    templateUrl: './event-search.component.html',
+    styleUrls: ['./event-search.component.css']
 })
-export class EventSearchComponent {
+export class EventSearchComponent implements OnInit{
 
+  searchEventsForm = new FormGroup({
+      keyword: new FormControl(''),
+      distance: new FormControl(10),
+      location: new FormControl(''),
+      segment: new FormControl()
+  });
+
+  filteredEvents: any;
+  isLoading = false;
+  errorMsg!: string;
+  minLengthTerm = 1;
+  selectedEvent: any = "";
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+
+  onSelected() {
+    this.searchEventsForm.patchValue({'keyword' : this.selectedEvent});
+  }
+
+  displayWith(value: any) {
+    return value?.Title;
+  }
+
+  clearSelection() {
+    this.selectedEvent = "";
+    this.filteredEvents = [];
+  }
+
+  ngOnInit() {
+
+
+    this.searchEventsForm.get('keyword')?.valueChanges
+      .pipe(
+        filter(res => {
+          return res !== null && res.length >= this.minLengthTerm
+        }),
+        distinctUntilChanged(),
+        debounceTime(1000),
+        tap(() => {
+          this.errorMsg = "";
+          this.filteredEvents = [];
+          this.isLoading = true;
+        }),
+        switchMap(value => this.http.get('http://localhost:3000/suggest?keyword=' + value)
+          .pipe(
+            finalize(() => {
+              this.isLoading = false
+            }),
+          )
+        )
+      )
+      .subscribe((data: any) => {
+        if (data['events'] == undefined) {
+          this.errorMsg = data['Error'];
+          this.filteredEvents = [];
+        } else {
+          this.errorMsg = "";
+          this.filteredEvents = data['events'];
+        }
+      });
+  }
 }
