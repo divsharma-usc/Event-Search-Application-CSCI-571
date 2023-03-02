88ea17d44f49dd4a8e0efd3585a681e909f79bd5
Divyansh Sharma <sharmadi@usc.edu>
2023-03-01 20:54:13 -0800

    Added Navigation Bar

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { EventSearchComponent } from './event-search/event-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FavoritesComponent,
+    EventSearchComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/event-search/event-search.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'event-search',
+  templateUrl: './event-search.component.html',
+  styleUrls: ['./event-search.component.css']
+})
+export class EventSearchComponent {
+
+}
