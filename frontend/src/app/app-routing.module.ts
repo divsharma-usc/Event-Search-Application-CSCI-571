9c11b5414c133f6eccceaf9545aca00f24562182
Divyansh Sharma <sharmadi@usc.edu>
2023-03-30 08:10:04 -0700

    Added Routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EventSearchComponent } from './event-search/event-search.component';
+import { FavoritesComponent } from './favorites/favorites.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/search', pathMatch: 'full' },
+  { path: 'search', component: EventSearchComponent },
+  { path: 'favorites', component: FavoritesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
